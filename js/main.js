d839176bac36ca0913ae3e61d4f3e7d76410274c
ziv <ziv921@yahoo.com>
2015-09-16 16:00:59 -0400

    We just added local storage and created functions to store the data

--- js/main.js
+++ js/main.js
@@ -2,14 +2,34 @@
   var selText = $(this).text();
   $(this).parents('.btn-group').find('.dropdown-toggle').html(selText+' <span class="caret"></span>');
 }); */
-
 function findCostumes() {
-	console.log("hi");
+	
 	var costumeStyle = document.getElementById("costumeTypes").value;
-	console.log(costumeStyle);
 	var gender = document.querySelector('input[name = "gender"]:checked').value;
-	console.log(gender);
+	
+			
+	localStorageData();
+			
+	if(costumeStyle == "scary") {
+		if(gender == "male") {
+			alert("scary male");
+
+		} else if (gender =="female") {
+			alert("scary female");
+		} else if (gender == "other") {
+			alert("scary other");
+		}
+	}
 }
 
-
+function localStorageData() {
+	var costumeStyle = document.getElementById("costumeTypes").value;
+	
+	var gender = document.querySelector('input[name = "gender"]:checked').value;
+	
+	// Store
+	localStorage.setItem("costumeTypes", costumeStyle);
+	localStorage.setItem("genderTypes", gender);
+}
+		
 
