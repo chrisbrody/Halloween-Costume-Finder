7e9324ec29dee19dec16392923b35ce83beda569
abby-garcia <acg361@gmail.com>
2015-09-16 15:22:45 -0400

    created on submit function

--- js/main.js
+++ js/main.js
@@ -1,6 +1,15 @@
-$(".dropdown-menu li a").click(function(){
+/*$(".dropdown-menu li a").click(function(){
   var selText = $(this).text();
   $(this).parents('.btn-group').find('.dropdown-toggle').html(selText+' <span class="caret"></span>');
-});
+}); */
+
+function findCostumes() {
+	console.log("hi");
+	var costumeStyle = document.getElementById("costumeTypes").value;
+	console.log(costumeStyle);
+	var gender = document.querySelector('input[name = "gender"]:checked').value;
+	console.log(gender);
+}
+
 
 
