3eeb780ecfe37bc610462c42549a78d8e43c3e31
ziv <ziv921@yahoo.com>
2015-09-19 15:32:23 -0400

    Guys we are done yayyy hurrayy

--- js/main.js
+++ js/main.js
@@ -27,7 +27,102 @@ function localStorageData() {
 function showResults() {
 	var costumeStyle = localStorage.costumeTypes;
 	var costumeGender = localStorage.genderTypes;
-	console.log(costumeStyle == "superhero")
+	
+
+
+//scary//
+if(costumeStyle == "scary") {
+		if(costumeGender == "male") {
+			document.getElementById("mscary").className =
+   document.getElementById("mscary").className.replace("hidden", "");
+   console.log(document.getElementsByClassName("mscary").className);
+
+
+		} else if (costumeGender  == "female") {	
+			document.getElementById("fscary").className =
+   document.getElementById("fscary").className.replace("hidden", "");
+   console.log(document.getElementsByClassName("fscary").className);
+		}
+	}
+//FUNNY//
+if(costumeStyle == "funny") {
+		if(costumeGender == "male") {
+			document.getElementById("mfunny").className =
+   document.getElementById("mfunny").className.replace("hidden", "");
+   console.log(document.getElementsByClassName("mfunny").className);
+
+
+		} else if (costumeGender  == "female") {
+			document.getElementById("ffunny").className =
+   document.getElementById("ffunny").className.replace("hidden", "");
+   console.log(document.getElementsByClassName("mfunny").className);
+		}
+	}
+
+//ANIMALS//
+
+if(costumeStyle == "animals") {
+		if(costumeGender == "male") {
+			document.getElementById("manimal").className =
+   document.getElementById("manimal").className.replace("hidden", "");
+   console.log(document.getElementsByClassName("manimal").className);
+
+
+		} else if (costumeGender  == "female") {
+			document.getElementById("fanimal").className =
+   document.getElementById("fanimal").className.replace("hidden", "");
+   console.log(document.getElementsByClassName("fanimal").className);
+		}
+	}
+
+	//SEXY//
+
+	if(costumeStyle == "sexy") {
+		if(costumeGender == "male") {
+			document.getElementById("msexy").className =
+   document.getElementById("msexy").className.replace("hidden", "");
+   console.log(document.getElementsByClassName("msexy").className);
+
+
+		} else if (costumeGender  == "female") {
+		document.getElementById("fsexy").className =
+   document.getElementById("fsexy").className.replace("hidden", "");
+   console.log(document.getElementsByClassName("fsexy").className);
+		}
+	}
+
+	//MOVIES//
+
+	if(costumeStyle == "movie") {
+		if(costumeGender == "male") {
+			document.getElementById("mmovie").className =
+   document.getElementById("mmovie").className.replace("hidden", "");
+   console.log(document.getElementsByClassName("mmovie").className);
+
+
+		} else if (costumeGender  == "female") {
+		document.getElementById("fmovie").className =
+   document.getElementById("fmovie").className.replace("hidden", "");
+   console.log(document.getElementsByClassName("fmovie").className);
+		}
+	}
+//MEDIVAL//
+
+if(costumeStyle == "medival") {
+		if(costumeGender == "male") {
+			document.getElementById("mmedival").className =
+   document.getElementById("mmedival").className.replace("hidden", "");
+   console.log(document.getElementsByClassName("mmedival").className);
+
+
+		} else if (costumeGender  == "female") {
+		document.getElementById("fmedival").className =
+   document.getElementById("fmedival").className.replace("hidden", "");
+   console.log(document.getElementsByClassName("fmedival").className);
+		}
+	}
+
+//SUPERHERO//
 	if(costumeStyle == "superhero") {
 		if(costumeGender == "male") {
 			document.getElementById("msuperhero").className =
@@ -36,9 +131,9 @@ function showResults() {
 
 
 		} else if (costumeGender  == "female") {
-			alert("superhero female");
-		} else if (costumeGender  == "other") {
-			alert("superhero other");
+		document.getElementById("fsuperhero").className =
+   document.getElementById("fsuperhero").className.replace("hidden", "");
+   console.log(document.getElementsByClassName("fsuperhero").className);
 		}
 	}
 }
